a252c5ade8490d2a70e7e8709248cf669902056a
Anderson Santos Gusmão <anderson.santos.gusmao@gmail.com>
2017-11-07 21:43:33 -0200

    ...

---
+++ CrackingTheCodingInterview/CrackingTheCodingInterview/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CrackingTheCodingInterview
+//
+//  Created by Anderson Santos Gusmao on 07/11/17.
+//  Copyright © 2017 Heuristisk. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
