5dba0e86b3a1601376ba044d5d7455da030faea4
Anderson Santos Gusmão <anderson.santos.gusmao@gmail.com>
2017-11-07 23:47:57 -0200

    ...

--- CrackingTheCodingInterview/CrackingTheCodingInterview/main.swift
+++ CrackingTheCodingInterview/CrackingTheCodingInterview/main.swift
@@ -10,3 +10,23 @@ import Foundation
 
 print("Hello, World!")
 
+func reverseParentheses(s: String) -> String {
+
+    var s = s
+
+    if let a = s.range(of: "(") {
+        s =  s.substring(to: a.lowerBound) + reverseParentheses(s: s.substring(from: a.upperBound))
+        print(s)
+    }
+
+    if let b = s.range(of: ")") {
+        s =  String(s.substring(to: b.lowerBound).characters.reversed()) + s.substring(from: b.upperBound)
+    }
+
+    return s
+}
+
+
+var s = "Code(Cha(lle)nge)"
+reverseParentheses(s: s)
+//print("CodeegnlleahC")
